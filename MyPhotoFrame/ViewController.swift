88f14e246d7808600645f9ab38c77d0a97ccc288
Kenya Yasumura <kennyyasumura@yahoo.co.jp>
2020-08-28 21:58:21 +0900

    Initial Commit

---
+++ MyPhotoFrame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPhotoFrame
+//
+//  Created by 安村建哉 on 2020/08/28.
+//  Copyright © 2020 安村建哉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
